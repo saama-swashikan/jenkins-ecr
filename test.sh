a6acef6d5ca327706190655830ca05ae09bc715e
Swashikan <s.sampooranam@saama.com>
2023-11-10 20:47:25 +0530

    adds sdq-sam to pull and push

--- test.sh
+++
@@ -1,8 +0,0 @@
- aws ecr describe-images --repository-name swashikan --region us-east-1  > new.txt
-word=new
- cat new.txt
-if grep -wq $word new.txt; then 
-    echo "Exists" 
-else 
-    echo "Does not exist"
-fi
