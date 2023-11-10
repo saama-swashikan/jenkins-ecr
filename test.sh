cdb10e2e95fa2c815a03c4a8db9f1ddc20079d14
Swashikan <s.sampooranam@saama.com>
2023-11-10 16:51:08 +0530

    adds condtion

--- test.sh
+++ test.sh
@@ -1,5 +1,5 @@
-aws ecr describe-images --repository-name swashikan --region us-east-1 --output json | jq -r '.imageDetails.[].imageTags.[]' > new.txt
-if grep -wq "new" new.txt; then 
+aws ecr describe-images --repository-name swashikan --region us-east-1 | grep  'IMAGETAGS' > new.txt
+if grep -wq "new234" new.txt; then 
     echo "Exists" 
 else 
     echo "Does not exist"
