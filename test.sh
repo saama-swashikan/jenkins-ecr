08d72296eb2eaeb350847badf1be3e03c4ae363c
Swashikan <s.sampooranam@saama.com>
2023-11-10 17:06:04 +0530

    adds condtion

--- test.sh
+++ test.sh
@@ -1,5 +1,6 @@
-aws ecr describe-images --repository-name swashikan --region us-east-1 | grep  'IMAGETAGS' > new.txt
-if grep -wq "new234" new.txt; then 
+# aws ecr describe-images --repository-name swashikan --region us-east-1  > new.txt
+# cat new.txt
+if grep -wq "new" new.txt; then 
     echo "Exists" 
 else 
     echo "Does not exist"
