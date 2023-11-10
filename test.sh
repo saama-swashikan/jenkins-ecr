0af9587733c30a29f278e37e74453e12236c1ad0
Swashikan <s.sampooranam@saama.com>
2023-11-10 18:08:18 +0530

    adds condtion

--- test.sh
+++ test.sh
@@ -1,5 +1,5 @@
-# aws ecr describe-images --repository-name swashikan --region us-east-1  > new.txt
-# cat new.txt
+ aws ecr describe-images --repository-name swashikan   > new.txt
+ cat new.txt
 if grep -wq "new" new.txt; then 
     echo "Exists" 
 else 
